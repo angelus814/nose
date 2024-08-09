64bce28c0b996f804954f01a22f0fef45b41b56d
angelus814 <ruizvaria814@gmail.com>
2024-08-09 17:01:06 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,47 @@
+let nombre = document.querySelector(".nombre");
+let mensaje = document.getElementById("mensaje");
+const boton = document.getElementById("boton");
+const bottonLoggin = document.getElementById("loggin")
+const usuario = document.getElementById("usuario")
+let contenedorFixed = document.querySelector(".fixed")
+// Cargar los comentarios existentes desde localStorage
+let arrae = JSON.parse(localStorage.getItem("msj")) || [];
+let indice = arrae.length;  // Para mantener el índice correctamente
+let comentario = document.querySelector(".comentarios")
+
+
+function datos() {
+    let dato = { name: usuario.value, msj: mensaje.value };
+    arrae.push(dato);
+    localStorage.setItem("msj", JSON.stringify(arrae));
+}
+
+let msj = JSON.parse(localStorage.getItem("msj"))
+function contenido (){
+    msj.forEach(element => {
+        comentario.innerHTML += 
+    `
+        <div class="comentario">
+            <p class="nombreComentario">@${element.name}</p>
+            <p class="mensajeComentario">${element.msj}</p>
+        </div>
+     `
+    });
+    
+}
+contenido()
+boton.addEventListener("click", () => {
+    datos();
+    comentario.innerHTML += 
+    `
+        <div class="comentario">
+            <p class="nombreComentario">@${usuario.value}</p>
+            <p class="mensajeComentario">${mensaje.value}</p>
+        </div>
+     `
+});
+bottonLoggin.addEventListener("click", ()=>{
+    contenedorFixed.classList.toggle("none")
+    nombre.textContent = `${usuario.value}`
+    console.log(JSON.parse(localStorage.getItem("msj")))
+})
